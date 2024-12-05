d2ce37b92dc41d2135dd6fc2c198f4e4445732c7
andrewkraevskii <andrew.kraevskii@gmail.com>
2024-12-05 11:42:16 +0300

    Day 5 part 1

--- src/day-5/solution.zig
+++ src/day-5/solution.zig
@@ -1,79 +1,43 @@
 const std = @import("std");
 
+pub const Entry = extern struct {
+    num: u16 align(1),
+    _: u8,
+
+    pub fn format(value: Entry, comptime _: []const u8, _: std.fmt.FormatOptions, writer: anytype) !void {
+        try writer.print("{d}", .{value.num});
+    }
+};
+
 pub fn solution1(_: std.mem.Allocator, text: []const u8) !u64 {
-    const width = std.mem.indexOfScalar(u8, text, '\n') orelse return error.NoLineBreak;
+    const end = (std.mem.indexOf(u8, text, "\n\n") orelse return error.MalformedInput) + 1;
+    const mapping = std.mem.bytesAsSlice([2]Entry, text[0..end]);
 
-    const stride = width + 1; // +1 for \n
-    const strides = [_]isize{
-        1,
-        -1,
-        @as(isize, @intCast(stride)),
-        @as(isize, @intCast(stride + 1)),
-        @as(isize, @intCast(stride - 1)),
-        -@as(isize, @intCast(stride)),
-        -@as(isize, @intCast(stride + 1)),
-        -@as(isize, @intCast(stride - 1)),
-    };
     var counter: u64 = 0;
-    for (strides) |s| {
-        counter += countText(text, "XMAS", s);
-    }
-    return counter;
-}
+    var pos = end + 1;
+    outer: while (true) {
+        const end_of_line = std.mem.indexOfScalarPos(u8, text, pos, '\n') orelse break;
+        defer pos = end_of_line + 1;
 
-fn containsStride(slice: []const u8, needle: []const u8, start: usize, stride: isize) bool {
-    var index: usize = 0;
-    var pos: isize = @intCast(start);
-    while (true) : ({
-        index += 1;
-        if (index >= needle.len) return true;
-        pos += stride;
-        if (pos < 0 or pos >= slice.len) return false;
-    }) {
-        if (needle[index] != slice[@intCast(pos)]) return false;
-    }
-}
-
-fn countText(slice: []const u8, needle: []const u8, stride: isize) u64 {
-    var counter: u64 = 0;
-    for (0..slice.len) |start| {
-        if (containsStride(slice, needle, start, stride))
-            counter += 1;
+        const list = std.mem.bytesAsSlice(Entry, text[pos .. end_of_line + 1]);
+        for (list[0 .. list.len - 1], 1..) |first, index| {
+            for (list[index..]) |second| {
+                for (mapping) |map| {
+                    if (map[1].num == first.num and map[0].num == second.num) {
+                        continue :outer;
+                    }
+                }
+            }
+        }
+        // NOTE: If I move this line before for loop runtime gets flower by 30%.
+        // But if measued using poop time is the same. It's really sus.
+        const middle = list[list.len >> 1].num;
+        const digits: [2]u8 = @bitCast(middle);
+        counter += 10 * (digits[0] - '0') + digits[1] - '0';
     }
     return counter;
 }
 
 pub fn solution2(_: std.mem.Allocator, text: []const u8) !u64 {
-    const width = std.mem.indexOfScalar(u8, text, '\n') orelse return error.NoLineBreak;
-    const height = std.mem.count(u8, text, "\n");
-
-    const stride = width + 1; // +1 for \n
-    const strides = [_]isize{
-        @as(isize, @intCast(stride + 1)),
-        @as(isize, @intCast(stride - 1)),
-        -@as(isize, @intCast(stride + 1)),
-        -@as(isize, @intCast(stride - 1)),
-    };
-
-    var counter: usize = 0;
-    for (1..width - 1) |start_x| {
-        for (1..height - 1) |start_y| {
-            var mases_in_x: u2 = 0;
-            for (strides) |s| {
-                const start: isize = @intCast(stride * start_y + start_x);
-                const offseted_start = start - s;
-                if (offseted_start < 0 or offseted_start >= text.len) continue;
-                mases_in_x += @intFromBool(containsStride(
-                    text,
-                    "MAS",
-                    @intCast(offseted_start),
-                    s,
-                ));
-            }
-
-            counter += @intFromBool(mases_in_x == 2);
-        }
-    }
-
-    return counter;
+    _ = text; // autofix
 }
