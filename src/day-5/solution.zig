723061b92eb6c74ba758d46a8ca8687559a3c50c
andrewkraevskii <andrew.kraevskii@gmail.com>
2024-12-05 13:50:01 +0300

    Day 5: unstable sort is super slow for some reason

--- src/day-5/solution.zig
+++ src/day-5/solution.zig
@@ -9,13 +9,10 @@ pub const Entry = extern struct {
     }
 };
 
-pub fn parseNum(num: [4]u8) u16 {
-    return (@as(u16, (num[0] - '0')) * 10 +
-        num[1] - '0') * 100 +
-        (num[2] - '0') * 10 +
-        num[3] - '0';
+pub fn parseNum(num: [2]u8) u16 {
+    return (num[0] - '0') * 10 +
+        num[1] - '0';
 }
-
 pub fn solution1(_: std.mem.Allocator, text: []const u8) !u64 {
     const end = (std.mem.indexOf(u8, text, "\n\n") orelse return error.MalformedInput) + 1;
     const mapping = mapping: {
@@ -55,12 +52,13 @@ pub fn solution1(_: std.mem.Allocator, text: []const u8) !u64 {
 pub fn solution2(_: std.mem.Allocator, text: []const u8) !u64 {
     const end = (std.mem.indexOf(u8, text, "\n\n") orelse return error.MalformedInput) + 1;
     const mapping = mapping: {
-        var bools: [100 * 100]bool = @splat(false);
+        var bools: [100][100]bool = @splat(@splat(false));
         const mapping = std.mem.bytesAsSlice([2]Entry, text[0..end]);
 
         for (mapping) |entry| {
-            const num = parseNum(entry[0].num ++ entry[1].num);
-            bools[num] = true;
+            const num = parseNum(entry[0].num);
+            const second = parseNum(entry[1].num);
+            bools[num][second] = true;
         }
         break :mapping bools;
     };
@@ -68,44 +66,42 @@ pub fn solution2(_: std.mem.Allocator, text: []const u8) !u64 {
     var counter: u64 = 0;
     var pos = end + 1;
 
-    var bounded_array = std.BoundedArray([2]u8, 100){};
+    var bounded_array = std.BoundedArray(u16, 100){};
     while (true) {
+        bounded_array.clear();
+
         const end_of_line = std.mem.indexOfScalarPos(u8, text, pos, '\n') orelse break;
         defer pos = end_of_line + 1;
 
         const entries = std.mem.bytesAsSlice(Entry, text[pos .. end_of_line + 1]);
-        bounded_array.clear();
         for (entries) |entry| {
-            try bounded_array.append(entry.num);
+            try bounded_array.append(parseNum(entry.num));
         }
 
         const list = bounded_array.slice();
         not_sorted: {
             for (list[0 .. list.len - 1], 1..) |first, index| {
                 for (list[index..]) |second| {
-                    const entry: u32 = parseNum(second ++ first);
-                    if (mapping[entry]) {
+                    if (mapping[second][first]) {
                         break :not_sorted;
                     }
                 }
             }
             continue;
         }
-        std.mem.sortUnstable(
-            [2]u8,
+        std.mem.sort(
+            u16,
             list,
             mapping,
             struct {
-                fn lessThan(m: @TypeOf(mapping), lhs: [2]u8, rhs: [2]u8) bool {
-                    const entry: u32 = parseNum(rhs ++ lhs);
-                    return m[entry];
+                fn lessThan(m: @TypeOf(mapping), lhs: u16, rhs: u16) bool {
+                    return m[lhs][rhs];
                 }
             }.lessThan,
         );
 
         const middle = list[list.len >> 1];
-        const digits: [2]u8 = middle;
-        counter += 10 * (digits[0] - '0') + digits[1] - '0';
+        counter += middle;
     }
     return counter;
 }
