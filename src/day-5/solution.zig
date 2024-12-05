45ede6d14dd99bf135f116a20b7898599644463f
andrewkraevskii <andrew.kraevskii@gmail.com>
2024-12-05 11:42:55 +0300

    Day 5: use hashmap

--- src/day-5/solution.zig
+++ src/day-5/solution.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 pub const Entry = extern struct {
-    num: u16 align(1),
+    num: [2]u8,
     _: u8,
 
     pub fn format(value: Entry, comptime _: []const u8, _: std.fmt.FormatOptions, writer: anytype) !void {
@@ -9,10 +9,21 @@ pub const Entry = extern struct {
     }
 };
 
-pub fn solution1(_: std.mem.Allocator, text: []const u8) !u64 {
+pub fn solution1(alloc: std.mem.Allocator, text: []const u8) !u64 {
     const end = (std.mem.indexOf(u8, text, "\n\n") orelse return error.MalformedInput) + 1;
-    const mapping = std.mem.bytesAsSlice([2]Entry, text[0..end]);
+    const mapping = mapping: {
+        const mapping = std.mem.bytesAsSlice([2]Entry, text[0..end]);
 
+        const packed_mapping = try alloc.alloc(u32, mapping.len);
+        for (packed_mapping, mapping) |*p, m| {
+            p.* = @bitCast(m[0].num ++ m[1].num);
+        }
+        break :mapping try std.AutoArrayHashMapUnmanaged(u32, void).init(
+            alloc,
+            packed_mapping,
+            undefined,
+        );
+    };
     var counter: u64 = 0;
     var pos = end + 1;
     outer: while (true) {
@@ -22,22 +33,17 @@ pub fn solution1(_: std.mem.Allocator, text: []const u8) !u64 {
         const list = std.mem.bytesAsSlice(Entry, text[pos .. end_of_line + 1]);
         for (list[0 .. list.len - 1], 1..) |first, index| {
             for (list[index..]) |second| {
-                for (mapping) |map| {
-                    if (map[1].num == first.num and map[0].num == second.num) {
-                        continue :outer;
-                    }
+                const entry: u32 = @bitCast(second.num ++ first.num);
+                if (mapping.contains(entry)) {
+                    continue :outer;
                 }
             }
         }
-        // NOTE: If I move this line before for loop runtime gets flower by 30%.
-        // But if measued using poop time is the same. It's really sus.
         const middle = list[list.len >> 1].num;
-        const digits: [2]u8 = @bitCast(middle);
+        const digits: [2]u8 = middle;
         counter += 10 * (digits[0] - '0') + digits[1] - '0';
     }
     return counter;
 }
 
-pub fn solution2(_: std.mem.Allocator, text: []const u8) !u64 {
-    _ = text; // autofix
-}
+pub fn solution2(_: std.mem.Allocator, _: []const u8) !u64 {}
