07f50775aaa046023d5be4c770d9da52fb1bf41e
andrewkraevskii <andrew.kraevskii@gmail.com>
2024-12-08 16:08:26 +0300

    Day 8 part 1

---
+++ src/day-8/solution.zig
@@ -0,0 +1,54 @@
+const std = @import("std");
+
+const Vec = @Vector(2, u8);
+
+fn addDiff(a: Vec, b: Vec, size: Vec) ?Vec {
+    const res, const overflow = @subWithOverflow(a + a, b);
+    if (@reduce(.Or, overflow != @as(Vec, @splat(0))) or
+        @reduce(.Or, res >= size)) return null;
+
+    return res;
+}
+
+pub fn solution1(arena: std.mem.Allocator, text: []const u8) !u64 {
+    const result = try arena.dupe(u8, text);
+    const width = std.mem.indexOfScalar(u8, text, '\n') orelse return error.NoLineBreak;
+    const height = std.mem.count(u8, text, "\n");
+    const size: Vec = .{ @intCast(width), @intCast(height) };
+
+    const stride = width + 1;
+
+    var frequencies: std.BoundedArray(u8, 0x30) = .{};
+    var positions: std.BoundedArray(std.BoundedArray(Vec, 10), 0x30) = .{};
+    for (text, 0..) |char, position| {
+        if (char == '\n' or char == '.') continue;
+        const index = if (std.mem.indexOfScalar(u8, frequencies.slice(), char)) |index| index else blk: {
+            try frequencies.append(char);
+            try positions.append(.{});
+            break :blk positions.len - 1;
+        };
+
+        try positions.slice()[index].append(.{ @intCast(position % stride), @intCast(position / stride) });
+    }
+    var counter: usize = 0;
+    for (positions.slice()) |tl| {
+        const tower_locations = tl.slice();
+
+        for (tower_locations[0 .. tower_locations.len - 1], 0..) |start, start_index| {
+            for (tower_locations[start_index + 1 ..]) |end| {
+                inline for (.{ .{ start, end }, .{ end, start } }) |points| {
+                    if (addDiff(points[0], points[1], size)) |pos| {
+                        const char = &result[pos[0] + pos[1] * stride];
+                        if (char.* != '#') {
+                            counter += 1;
+                            char.* = '#';
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return counter;
+}
+pub fn solution2(_: std.mem.Allocator, _: []const u8) !u64 {}
